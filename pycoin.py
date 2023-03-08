334a85259a1a2072832187793e99b2fb4d5a1eea
phineasphreak <riftpp@gmail.com>
2023-03-08 19:19:11 +0100

    Création du fichier data.csv avec génération de 5 pages, fonctionnel

--- pycoin.py
+++ pycoin.py
@@ -1,5 +1,105 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from pycoingecko import CoinGeckoAPI
-cg = CoinGeckoAPI()
+
+import requests
+import pandas as pd
+import urllib.error
+import rich
+
+
+# With this is that all errors will be ignored, therefore it is not ideal.
+# import warnings
+
+# RuntimeWarning: invalid value encountered in cast values = values.astype(str)
+# warnings.filterwarnings("ignore")
+
+def markets(
+    vs_currencies="usd",
+    order="market_cap_desc",
+    per_page=250,
+    page=1,
+    sparkline=False,
+):
+    """
+    List all supported coins price, market cap, volume,
+    and market related data
+    https://www.delftstack.com/howto/python-pandas/
+    """
+
+    cg_markets = (
+        f"https://api.coingecko.com/api/v3/coins/"
+        f"markets?vs_currency={vs_currencies}&"
+        f"order={order}&"
+        f"per_page={per_page}&"
+        f"page={page}&"
+        f"sparkline={sparkline}"
+    )
+
+    # Source all data in terminal
+    # answer_markets = requests.get(cg_markets).json()
+
+    # Convert json format on DataFrame in pandas
+    pd_markets = pd.read_json(cg_markets, orient="records", encoding="utf-8", dtype="objet")
+
+    # For delete column in DataFrame
+    # https://stackoverflow.com/questions/13411544/delete-a-column-from-a-pandas-dataframe
+    pd_markets = pd.DataFrame(
+        data=pd_markets,
+        columns=[
+            "id",
+            "symbol",
+            "name",
+            "current_price",
+            "market_cap",
+            "market_cap_rank",
+            "fully_diluted_valuation",
+            "total_volume",
+            "high_24h",
+            "low_24h",
+            "price_change_24h",
+            "price_change_percentage_24h",
+            "market_cap_change_24h",
+            "market_cap_change_percentage_24h",
+            "circulating_supply",
+            "total_supply",
+            "max_supply",
+            "last_updated",
+        ],
+    )
+
+    # Sets the 'market_cap_rank' column as an index of the my_df DataFrame
+    pd_markets_df_rank = pd_markets.set_index("market_cap_rank")
+
+    # pd_markets_df_rank.to_csv("data.csv", encoding="utf-8", index=False)
+
+    # Delete 'image' column in the data Frame
+    # pd_markets_df_rank.drop('image', axis=1, inplace=True)
+
+    return pd_markets_df_rank
+
+# Exemple sans boucle FOR
+# df1 = markets(category=False, page=1)
+# df2 = markets(category=False, page=2)
+
+# df_concat = pd.concat([df1, df2])
+# print(df_concat)
+# df_concat.to_csv("data.csv", index=False)
+
+
+# Concaténer plusieurs tableaux pandas ensemble
+# https://www.geeksforgeeks.org/convert-multiple-json-files-to-csv-python/
+# https://towardsdatascience.com/concatenate-multiple-and-messy-dataframes-efficiently-80847b4da12b
+number_of_page = 5
+dfs = []
+
+try:
+    for num_page in range(1, number_of_page + 1):
+        df_market = markets(page=num_page)
+        dfs.append(df_market)
+
+    df_concat = pd.concat(dfs)
+    df_concat.to_csv("data.csv", index=False)
+
+except urllib.error.HTTPError as err:
+    print(err)
